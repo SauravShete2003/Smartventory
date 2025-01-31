13a4d10e1fdb014f2b12402f9fe19c56c7f70105
sauravshete2003 <sauravshete72@gmail.com>
2025-01-31 18:39:59 +0530

    fix the error

--- server/index.js
+++ server/index.js
@@ -19,17 +19,23 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-
+app.use(express.static('dist', {
+  setHeaders: (res, path) => {
+      if (path.endsWith('.css')) {
+          res.setHeader('Content-Type', 'text/css');
+      }
+  }
+}));
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // or specify origin
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // include Authorization
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
   if (req.method === 'OPTIONS') {
-      res.sendStatus(200); // Important: Respond to preflight requests
+      res.sendStatus(200); 
   } else {
-      next(); // Continue to other routes
+      next();
   }
 });
 
