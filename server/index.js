7a6d3a42be48de31a4d8e55185e2bbd9e83c910a
sauravshete2003 <sauravshete72@gmail.com>
2025-01-31 19:29:19 +0530

    fix cors policy

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.static('dist', {
   }
 }));
 
-app.use(cors());
+app.use(cors({origin: 'https://smart-inventory-ysnz.onrender.com'}));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
