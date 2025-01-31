71568fdf208c3e140dade38e48152ac510bdd3a7
sauravshete2003 <sauravshete72@gmail.com>
2025-01-31 18:29:15 +0530

    add cors error correction

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import cors from "cors";
 import { configDotenv } from "dotenv";
 configDotenv();
@@ -18,27 +18,20 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-import cors from "cors";  // Import cors
 
-const corsOptions = {
-  origin: "https://smart-inventory-ysnz.onrender.com" || "localhost://5173",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-};
 
-app.use(cors(corsOptions)); // Use CORS middleware
 
-// app.use(cors());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*'); // or specify origin
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // include Authorization
-//   if (req.method === 'OPTIONS') {
-//       res.sendStatus(200); // Important: Respond to preflight requests
-//   } else {
-//       next(); // Continue to other routes
-//   }
-// });
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // or specify origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // include Authorization
+  if (req.method === 'OPTIONS') {
+      res.sendStatus(200); // Important: Respond to preflight requests
+  } else {
+      next(); // Continue to other routes
+  }
+});
 
 const mongoDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URL);
